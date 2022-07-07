27a459f9871189cdb66b8218f1c8e7ec209f0585
zhqmfkv <zhqmfkv@naver.com>
2022-07-07 19:40:29 +0900

    [FIX] 회원가입

--- user/serializer.py
+++ user/serializer.py
@@ -1,6 +1,4 @@
 from rest_framework import serializers
-
-# from blog.serializer import ArticleSerializer
 from user.models import User, UserProfile
 
 
@@ -10,13 +8,32 @@ class UserProfileSerializer(serializers.ModelSerializer):
         fields = ['bio']
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSignUpSerializer(serializers.ModelSerializer):
     userprofile = UserProfileSerializer()
-    # article_set = ArticleSerializer(many=True)
+
+    def validate(self, validated_data):
+        email = validated_data.get('email')
+        user_query = User.objects.filter(email=email)
+        if user_query.exists():
+            raise serializers.ValidationError
+        return validated_data
 
     class Meta:
         model = User
-        fields = ['username', 'password', 'fullname', 'email', 'userprofile', 'article_set']
+        fields = ['username', 'password', 'fullname', 'email', 'userprofile']
+        extra_kwargs = {
+            'password': {
+                'write_only': True
+            },
+        }
+
+
+class UserSerializer(serializers.ModelSerializer):
+    userprofile = UserProfileSerializer()
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'fullname', 'email', 'userprofile']
         extra_kwargs = {
             'password': {
                 'write_only': True
--- user/views.py
+++ user/views.py
@@ -5,7 +5,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from user.models import User, UserProfile
-from user.serializer import UserSerializer
+from user.serializer import UserSignUpSerializer, UserSerializer
 
 
 @swagger_auto_schema(tag='유저_회원 가입')
@@ -16,51 +16,10 @@ class UserSignupView(APIView):
         :param request:
         :return: Json
         """
-        # 응답 data set
-        data = {
-            'message': 'Fail',
-            'detail': ''
-        }
-
-        # request 로부터 데이터 가져오기
-        user_name = request.data.get('username', None)
-        user_password = request.data.get('password', None)
-        user_email = request.data.get('email', None)
-        user_fullname = request.data.get('name', None)
-        user_bio = request.data.get('bio', '')
-
-        # 회원 가입 로직 시작
-        try:
-            # 아이디 중복 처리
-            if User.objects.filter(username=user_name).exists():
-                data['detail'] = '아이디 중복'
-                return JsonResponse(data)
-
-            user, created = User.objects.get_or_create(
-                username=user_name,
-                email=user_email,
-                fullname=user_fullname
-                )
-            user.set_password(user_password)
-
-            # 모든 정보가 똑같은 유저
-            if created is False:
-                data['detail'] = '이미 존재 하는 유저'
-                return JsonResponse(data)
-
-            # 새로운 회원일 경우
-            else:
-                user.save()
-                profile = UserProfile.objects.create(user=user, bio=user_bio)
-                profile.save()
-                data['message'] = 'success'
-                data['detail'] = user.username
-                return JsonResponse(data)
-
-        # 이외의 예외 처리
-        except Exception as e:
-            data['detail'] = e.args[0]
-            return JsonResponse(data)
+        # 유저 회원 만들기
+        serializer = UserSignUpSerializer(data=request.data)
+        if serializer.is_valid() is False:
+            return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @swagger_auto_schema(tags=['유저_로그인'])
